38ff601f8c30ba814d707bb3fe35f8b8f5d97e78
zurez <zurez4u@gmail.com>
2016-04-27 13:58:20 +0530

    Init

---
+++ database/migrations/2016_04_27_075914_create_patient_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('patient',function(Blueprint $table){
+            $table->increment('id');
+            $table->string('address_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
